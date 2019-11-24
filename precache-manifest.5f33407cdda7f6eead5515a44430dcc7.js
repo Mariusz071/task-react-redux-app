e8c1eb8ceb5fa4dda0bcf8ab85fcd34d9b9bc1a7
Mariusz S <siewruk.mariusz92@gmail.com>
2019-11-24 12:57:31 +0000

    Updates

---
+++ precache-manifest.5f33407cdda7f6eead5515a44430dcc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa44b7da33a236d1c42a9f3791ab557",
+    "url": "/task-react-redux-app/index.html"
+  },
+  {
+    "revision": "fb8a1892405959170ce5",
+    "url": "/task-react-redux-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0f8aecf4fbdd020cf490",
+    "url": "/task-react-redux-app/static/js/2.fb681d1d.chunk.js"
+  },
+  {
+    "revision": "fb8a1892405959170ce5",
+    "url": "/task-react-redux-app/static/js/main.a544a4a1.chunk.js"
+  },
+  {
+    "revision": "99c53d57e36e728069d5",
+    "url": "/task-react-redux-app/static/js/runtime-main.cdff157f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/task-react-redux-app/static/media/logo.25bf045c.svg"
+  }
+]);
