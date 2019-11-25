201d8c8e20d34a7b069381ae0d9880e93d1e3db3
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-25 21:06:00 +0000

    added DailyForecast component

---
+++ src/components/dailyForecast/DailyForecast.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './DailyForecast.scss'
+
+const DailyForecast = props => {
+  console.log(props)
+  return (
+    <div className="daily-forecast">
+      <h3>daily forecast</h3>
+    </div>
+  )
+}
+
+export default DailyForecast
