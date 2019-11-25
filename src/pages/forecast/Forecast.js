57f3718fdd94fbec564ae218369b78d0c3fd85a2
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-25 23:51:39 +0000

    rendering daily forecast

--- src/pages/forecast/Forecast.js
+++ src/pages/forecast/Forecast.js
@@ -2,24 +2,38 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import DailyForecast from 'components/dailyForecast'
+import icons from 'components/icons'
+
+import './Forecast.scss'
 
 class Forecast extends Component {
   componentWillMount() {
+    //redirects to / when there is no weather forecast loaded to the state
     const { history, weather } = this.props
     if (!weather.city) history.push('/')
   }
+
+
+
   render() {
     const { weather } = this.props
     const { city, list } = weather
 
+    //prevents from crashing when gointg straight to /weather url without typing any city
+    const isLoaded = !!Object.keys(weather).length
+
     return (
       <div className="forecast-page">
-        {city && (
+        {isLoaded && (
           <>
-            <h2>{city.name}</h2>
-            {list.map((day, key) => {
-              return <DailyForecast data={day} />
-            })}
+            <h2 className="forecast-page__header">{city.name}</h2>
+            <div className="forecast-page__forecast">
+              {list.map((day, key) => {
+                const iconCode = day.weather[0].icon
+                const icon = icons[iconCode]
+                return <DailyForecast data={day} icon={icon} key={key} />
+              })}
+            </div>
           </>
         )}
       </div>
@@ -28,9 +42,6 @@ class Forecast extends Component {
 }
 
 const mapStateToProps = state => ({
-  weather: state.weather,
+  weather: state.weather
 })
-export default connect(
-  mapStateToProps,
-  null
-)(Forecast)
+export default connect(mapStateToProps, null)(Forecast)
