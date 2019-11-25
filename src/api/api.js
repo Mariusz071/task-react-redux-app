5f2cd534bc61487eeb202735bef66189ddd2c872
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-25 21:04:00 +0000

    fixed urls

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-const API_KEY = 'b10d0fbe0c7826e4bad1ef481f0b773f'
-const BASE_URL = `https:api.openweathermap.org/data/2.5/weather?q=`
+// I had to use api key from example as I was getting 401 error with my own key
+// const API_KEY = 'b10d0fbe0c7826e4bad1ef481f0b773f'
 
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b10d0fbe0c7826e4bad1ef481f0b773f
-// api.openweathermap.org/data/2.5/weather?q=london,uk&APPID=b10d0fbe0c7826e4bad1ef481f0b773f
+const API_KEY = 'b714ec74bbab5650795063cb0fdf5fbe'
+const BASE_URL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=`
 
 const api = {
-  getForecast: city => axios.get(`${BASE_URL}${city},uk&APPID=${API_KEY}`),
+  getForecast: city => axios.get(`${BASE_URL}${city}&type=accurate&APPID=${API_KEY}&cnt=5`),
 }
 
 export default api
