58e43ac2728fd2d69879b77507f8e1f264838306
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-25 21:05:32 +0000

    tweaked

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import debounce from 'lodash.debounce'
+// import debounce from 'lodash.debounce'
 
 import { connect } from 'react-redux'
 import { getWeather } from './ducks/actions'
@@ -12,19 +12,19 @@ class Search extends Component {
     city: '',
   }
 
-  getCity = city => {
-    const { getWeather } = this.props
-    this.setState({ city }, () => getWeather(city))
-  }
+  // getCity = city => {
+  //   const { getWeather } = this.props
+  //   this.setState({ city }, () => getWeather(city))
+  // }
 
-  debouncedGetCity = debounce(this.getCity, 300)
+  // debouncedGetCity = debounce(this.getCity, 300)
 
   render() {
-    console.log(this.state.city)
+    const { getWeather } = this.props
     return (
       <div className="search-page">
         <h2 className="search-page__header">Enter a City and State</h2>
-        <Searchbar display="vertical" getValue={this.debouncedGetCity} />
+        <Searchbar display="vertical" getValue={getWeather} />
       </div>
     )
   }
@@ -34,4 +34,7 @@ const mapDispatchToProps = {
   getWeather,
 }
 
-export default connect(null, mapDispatchToProps)(Search)
+export default connect(
+  null,
+  mapDispatchToProps
+)(Search)
