f0cee3320a2e066b984e1ee423c775981f8045dc
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-25 00:50:58 +0000

    added navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Searchbar from 'components/searchbar'
+import './Navbar.scss'
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h1 className="navbar__title">React-redux Weather App</h1>
+    <Searchbar />
+  </nav>
+)
+
+export default Navbar
