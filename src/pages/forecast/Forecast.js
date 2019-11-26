d8936f99dcc8dcb1be6ea70707d5b65edb189bbf
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-26 21:21:02 +0000

    added Forecast page

---
+++ src/pages/forecast/Forecast.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import DailyForecast from 'components/dailyForecast'
+import icons from 'components/icons'
+
+import './Forecast.scss'
+
+class Forecast extends Component {
+  componentWillMount() {
+    const { history, weather } = this.props
+    if (!weather.city) history.push('/')
+  }
+
+  render() {
+    const { weather } = this.props
+    const { city, list } = weather
+    const isLoaded = !!Object.keys(weather).length
+
+    return (
+      <div className="forecast-page">
+        {isLoaded && (
+          <>
+            <h2 className="forecast-page__header">{city.name}</h2>
+            <div className="forecast-page__forecast">
+              {list.map((day, key) => {
+                const iconCode = day.weather[0].icon
+                const icon = icons[iconCode]
+                return <DailyForecast data={day} icon={icon} key={key} id={day.dt} />
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    weather: state.weather,
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(Forecast)
---
+++ src/pages/forecast/index.js
@@ -0,0 +1,6 @@
+import Forecast from './Forecast'
+import reducer from './ducks/reducer'
+import * as actions from './ducks/actions'
+
+export { reducer, actions }
+export default Forecast
