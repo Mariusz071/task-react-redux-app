5351b5ca7450bf318523171f4809fcc24b4a603c
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-26 22:52:42 +0000

    added param for metric units

--- src/api/api.js
+++ src/api/api.js
@@ -7,7 +7,7 @@ const API_KEY = 'b714ec74bbab5650795063cb0fdf5fbe'
 const BASE_URL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=`
 
 const api = {
-  getForecast: city => axios.get(`${BASE_URL}${city}&type=accurate&APPID=${API_KEY}&cnt=5`),
+  getForecast: city => axios.get(`${BASE_URL}${city}&type=accurate&units=metric&APPID=${API_KEY}&cnt=5`),
 }
 
 export default api
