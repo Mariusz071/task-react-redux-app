4368f137a312eb4af41c00a54045baac24d9b144
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-26 21:30:45 +0000

    changed into functional component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,24 +3,11 @@ import Searchbar from 'components/searchbar'
 
 import './Navbar.scss'
 
-class Navbar extends Component {
-  render() {
-    const { getWeather } = this.props
-    return (
-      <nav className="navbar">
-        <h1 className="navbar__title">React-redux Weather App</h1>
-        <Searchbar getValue={getWeather} />
-      </nav>
-    )
-  }
-}
+const Navbar = () => (
+  <nav className="navbar">
+    <h1 className="navbar__title">React-redux Weather App</h1>
+    <Searchbar />
+  </nav>
+)
 
-const mapDispatchToProps = {
-  getWeather,
-}
-
-const mapStateToProps = state => ({
-  weather: state.weather,
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
+export default Navbar
