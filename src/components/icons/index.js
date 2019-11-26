37cde0ac1a93c1b50fc754c98a45e59d592fd210
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-26 21:22:36 +0000

    added icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,41 @@
+import ClearDayIcon from 'components/icons/ClearDay'
+import ClearNightIcon from 'components/icons/ClearNight'
+import MediumCloudyDayIcon from 'components/icons/MediumCloudyDay'
+import MediumCloudyNightIcon from 'components/icons/MediumCloudyNight'
+import CloudyDayIcon from 'components/icons/CloudyDay'
+import CloudyNightIcon from 'components/icons/CloudyNight'
+import HeavyCloudyDayIcon from 'components/icons/HeavyCloudyDay'
+import HeavyCloudyNightIcon from 'components/icons/HeavyCloudyNight'
+import RainyDayIcon from 'components/icons/RainyDay'
+import HeavyRainySunnyDayIcon from 'components/icons/HeavyRainySunnyDay'
+import RainySunnyDayIcon from 'components/icons/RainySunnyDay'
+import HeavyRainyNightIcon from 'components/icons/HeavyRainyNight'
+import StormyDayIcon from 'components/icons/StormyDay'
+import StormyNightIcon from 'components/icons/StormyNight'
+import SnowyNightIcon from 'components/icons/SnowyNight'
+import HeavySnowyNightIcon from 'components/icons/HeavySnowyNight'
+import FoggyDayIcon from 'components/icons/FoggyDay'
+import FoggyNightIcon from 'components/icons/FoggyNight'
+
+const icons = {
+  '01d': ClearDayIcon,
+  '01n': ClearNightIcon,
+  '02d': MediumCloudyDayIcon,
+  '02n': MediumCloudyNightIcon,
+  '03d': CloudyDayIcon,
+  '03n': CloudyNightIcon,
+  '04d': HeavyCloudyDayIcon,
+  '04n': HeavyCloudyNightIcon,
+  '09d': RainyDayIcon,
+  '09n': HeavyRainySunnyDayIcon,
+  '10d': RainySunnyDayIcon,
+  '10n': HeavyRainyNightIcon,
+  '11d': StormyDayIcon,
+  '11n': StormyNightIcon,
+  '13d': SnowyNightIcon,
+  '13n': HeavySnowyNightIcon,
+  '50d': FoggyDayIcon,
+  '50n': FoggyNightIcon,
+}
+
+export default icons
