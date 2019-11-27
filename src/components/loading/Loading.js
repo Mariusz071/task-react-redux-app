3fff75eefde51131280e5ec4809a2cefb32d52b3
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-27 23:14:14 +0000

    added Loading component

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SpinnerIcon from 'components/icons/Spinner'
+
+import './Loading.scss'
+
+const Loading = () => (
+  <div className="loading">
+    <h3>Loading</h3>
+    <SpinnerIcon />
+  </div>
+)
+
+export default Loading
