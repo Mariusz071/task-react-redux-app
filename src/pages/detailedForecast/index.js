84e2b03c461861b193e74fb223392085c5b6fe78
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-27 23:19:10 +0000

    imported actions and reducer

--- src/pages/detailedForecast/index.js
+++ src/pages/detailedForecast/index.js
@@ -1,3 +1,6 @@
 import DetailedForecast from './DetailedForecast'
+import reducer from './ducks/reducer'
+import * as actions from './ducks/actions'
 
+export { reducer, actions }
 export default DetailedForecast
