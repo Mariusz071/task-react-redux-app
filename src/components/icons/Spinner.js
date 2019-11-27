d0090927bc8489bb27385061a4276c8da8208c4f
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-27 23:13:52 +0000

    added Spinner icon

---
+++ src/components/icons/Spinner.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Icon = () => (
+  <svg width="24px" height="24px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+    <circle
+      cx="50"
+      cy="50"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="10"
+      r="36"
+      strokeDasharray="169.64600329384882 58.548667764616276"
+    >
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
+        calcMode="linear"
+        values="0 50 50;360 50 50"
+        keyTimes="0;1"
+        dur="1s"
+        begin="0s"
+        repeatCount="indefinite"
+      />
+    </circle>
+  </svg>
+)
+
+export default Icon
