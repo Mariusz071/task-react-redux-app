b7337510be67014fb887e5432bc15459dfcba54b
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-27 00:35:39 +0000

    catching error inside action

--- src/components/searchbar/ducks/actions.js
+++ src/components/searchbar/ducks/actions.js
@@ -1,11 +1,22 @@
 import api from 'api'
 import history from 'common/history'
+import get from 'lodash.get'
 
 export const getWeather = city => async dispatch => {
-  const res = await api.getForecast(city)
-  dispatch({
-    type: 'GET_WEATHER',
-    payload: res.data,
-  })
-  history.push(`/weather`)
+  try {
+    const res = await api.getForecast(city)
+
+    if (res.statusText === 'OK') {
+      dispatch({
+        type: 'GET_WEATHER',
+        payload: res.data,
+      })
+      return history.push(`/weather`)
+    }
+  } catch (error) {
+    dispatch({
+      type: 'ERROR',
+      error: get(error, 'response.data'),
+    })
+  }
 }
