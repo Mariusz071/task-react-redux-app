acb3a62b993d5bf545c74b50a876fa2b0fb83bcb
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-27 23:08:55 +0000

    removed comment

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,4 @@
 import axios from 'axios'
-
-// I had to use api key from example as I was getting 401 error with my own key
-// const API_KEY = 'b10d0fbe0c7826e4bad1ef481f0b773f'
-
 const API_KEY = 'b714ec74bbab5650795063cb0fdf5fbe'
 const BASE_URL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=`
 
