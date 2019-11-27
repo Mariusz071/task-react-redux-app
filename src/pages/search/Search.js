23b5ae86017a3a558967cef99a74f9043e61422b
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-27 23:17:13 +0000

    connected to redux-store, getting error message

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import get from 'lodash.get'
+
 import Searchbar from 'components/searchbar'
 
 import './Search.scss'
 
-const Search = () => (
-  <div className="search-page">
-    <h2 className="search-page__header">Enter a City and State</h2>
-    <Searchbar display="vertical" />
-  </div>
-)
+class Search extends Component {
+  render() {
+    const { error } = this.props
+    return (
+      <div className="search-page">
+        <h2 className="search-page__header">Enter a City and State</h2>
+        <Searchbar display="vertical" error={error} />
+      </div>
+    )
+  }
+}
 
-export default Search
+const mapStateToProps = state => ({
+  error: get(state, 'weather.error'),
+})
+
+export default connect(mapStateToProps, null)(Search)
