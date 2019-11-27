612b941ebedac470a1527e726e5813cd55641b16
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-27 23:09:42 +0000

    fixed redirect onClick

--- src/components/generalForecast/GeneralForecast.info.js
+++ src/components/generalForecast/GeneralForecast.info.js
@@ -5,11 +5,10 @@ import { formatDate } from 'common/utils'
 
 import './GeneralForecast.info.scss'
 
-const GeneralForecastInfo = ({ data, icon, id }) => {
+const GeneralForecastInfo = ({ data, icon, id, city }) => {
   const formattedDate = formatDate(data.dt)
-
   return (
-    <div className="general-forecast" onClick={() => history.push(`/weather/${id}`)}>
+    <div className="general-forecast" onClick={() => history.push(`/${city}/${id}`)}>
       {icon()}
       <p>{formattedDate}</p>
     </div>
