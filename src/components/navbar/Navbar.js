b4fda15a462a8b3d0c870165f4968fa62370fd1a
Mariusz <siewruk.mariusz92@gmail.com>
2019-11-27 23:15:21 +0000

    made title clickable

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
+
 import Searchbar from 'components/searchbar'
+import history from 'common/history'
 
 import './Navbar.scss'
 
 const Navbar = () => (
   <nav className="navbar">
-    <h1 className="navbar__title">React-redux Weather App</h1>
+    <div className="navbar__title" onClick={() => history.replace('/')}>
+      <h1>React-redux Weather App</h1>
+    </div>
     <Searchbar />
   </nav>
 )
