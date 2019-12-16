8b7d7983d437bde84cf5407835941772c8ecde11
Mariusz <siewruk.mariusz92@gmail.com>
2019-12-16 23:05:23 +0000

    added unit tests

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { mount } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from './App'
+import Navbar from 'components/navbar'
+import Search from 'pages/search'
+
+describe('the landing page', () => {
+  let component
+
+  beforeEach(() => (component = mount(<App />)))
+  afterEach(() => component.unmount())
+
+  it('renders navbar', () => {
+    expect(component.find(Navbar).length).toEqual(1)
+  })
+
+  it('renders search page', () => {
+    expect(component.find(Search).length).toEqual(1)
+  })
+})
---
+++ src/modules/searchbar/Searchbar.test.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import Searchbar from './Searchbar'
+import Root from 'Root'
+
+let component
+
+beforeEach(
+  () =>
+    (component = mount(
+      <Root>
+        <Searchbar />
+      </Root>
+    ))
+)
+
+afterEach(() => component.unmount())
+
+it('renders input and submit button', () => {
+  expect(component.find('input').length).toEqual(1)
+  expect(component.find('.btn').length).toEqual(1)
+})
+
+describe('the input field', () => {
+  let inputValue
+
+  beforeEach(() => {
+    component.find('input').simulate('change', { target: { value: 'London' } })
+    component.update()
+    inputValue = component.find('input').prop('value')
+  })
+
+  it('checks if input value is correct', () => {
+    expect(inputValue).toEqual('London')
+  })
+
+  it('checks if input value is erased on submit', () => {
+    const submitButton = component.find('.btn')
+    expect(inputValue).toEqual('London')
+    submitButton.simulate('click')
+    expect(component.find('input').prop('value')).toEqual('')
+  })
+})
