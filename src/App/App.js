17a170f42ab20741dae0ab75063927dba4cf186b
Mariusz <siewruk.mariusz92@gmail.com>
2019-12-16 23:05:55 +0000

    Added root wrapper - to fix provider error in unit tests

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from 'store'
-import history from 'common/history'
 
+import history from 'common/history'
+import Root from 'Root'
 import Layout from 'layout'
 import Navbar from 'components/navbar'
 import Search from 'pages/search'
@@ -12,21 +11,19 @@ import DetailedForecast from 'pages/detailedForecast'
 
 import './App.scss'
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <Layout>
-        <Router history={history}>
-          <Navbar />
-          <Switch>
-            <Route path="/" exact component={Search} />
-            <Route path="/:city/" exact component={Forecast} />
-            <Route path="/:city/:id" exact component={DetailedForecast} />
-          </Switch>
-        </Router>
-      </Layout>
-    </Provider>
-  )
-}
+const App = () => (
+  <Root>
+    <Layout>
+      <Router history={history}>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Search} />
+          <Route path="/:city/" exact component={Forecast} />
+          <Route path="/:city/:id" exact component={DetailedForecast} />
+        </Switch>
+      </Router>
+    </Layout>
+  </Root>
+)
 
 export default App
