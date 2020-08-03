eb300c30789fc1be3db030af6ffe126e0b6a1274
Mariusz <siewruk.mariusz92@gmail.com>
2020-08-04 00:22:38 +0200

    Moved logic to get daily weather into action creator

--- src/pages/detailedForecast/DetailedForecast.js
+++ src/pages/detailedForecast/DetailedForecast.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { getWeather } from './ducks/actions'
-import keyBy from 'lodash.keyby'
-import get from 'lodash.get'
 
-import DetailedForecastInfo from 'components/detailedForecast'
+import { getDailyWeather } from './ducks/actions'
+import { DetailedForecastInfo } from 'components/detailedForecast'
 import Loading from 'components/loading'
 
 import './DetailedForecast.scss'
@@ -15,34 +13,29 @@ class DetailedForecast extends Component {
   }
 
   async componentDidMount() {
-    const { getWeather, match } = this.props
-    await getWeather(match.params.city, match.params.id)
+    const { getDailyWeather, match } = this.props
+    await getDailyWeather(match.params.city, match.params.id)
     this.setState({ loading: false })
   }
 
   render() {
-    const { dailyWeather, city } = this.props
+    const { dailyWeather } = this.props
     const { loading } = this.state
 
     return (
       <div className="detailed-forecast-page">
-        {loading ? <Loading /> : <DetailedForecastInfo data={dailyWeather} city={city} />}
+        {loading ? <Loading /> : <DetailedForecastInfo data={dailyWeather} />}
       </div>
     )
   }
 }
 
 const mapDispatchToProps = {
-  getWeather,
+  getDailyWeather,
 }
-const mapStateToProps = (state, currentProps) => {
-  const weather = keyBy(get(state, 'weather.data.list'), 'dt')
-  const dayId = currentProps.match.params.id
-  return {
-    dailyWeather: get(weather, dayId),
-    city: get(state, 'weather.data.city.name'),
-  }
-}
+const mapStateToProps = (state, currentProps) => ({
+  dailyWeather: state.dailyWeather.data,
+})
 
 export default connect(
   mapStateToProps,
