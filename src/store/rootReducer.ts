d4a8b55bee91efd79c49a15c407c79a68a581710
Mariusz <siewruk.mariusz92@gmail.com>
2020-08-04 00:21:45 +0200

    Added dailyWeather reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { reducer as weather } from 'pages/forecast'
+import { reducer as dailyWeather } from 'pages/detailedForecast'
 
 export default combineReducers({
   weather,
+  dailyWeather
 })
