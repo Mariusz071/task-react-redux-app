57534b7aeb98430d6654bafe17af121ffe5ba247
Mariusz <siewruk.mariusz92@gmail.com>
2020-08-04 00:23:00 +0200

    Moved interface definitions into common/types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,9 +1,48 @@
-import { FC } from "react";
+interface Weather {
+    id: number,
+    main: string,
+    description: string,
+    icon: string
+}
 
 export interface RouteElement {
     path: string
-    component: FC
+    component: any,
     exact?: boolean
+}
+
+export enum ReduxActionTypes {
+    getWeather = 'GET_WEATHER',
+    error = 'ERROR',
+    getDailyWeather = 'GET_DAILY_WEATHER'
+}
+
+export interface DailyWeather {
+    dt: number,
+    sunrise: number,
+    sunset: number,
+    temp: {
+        day: number,
+        min: number,
+        max: number,
+        night: number,
+        eve: number,
+        morn: number
+    },
+    feels_like: {
+        day: number,
+        night: number,
+        eve: number,
+        morn: number
+    },
+    pressure: number,
+    humidity: number,
+    weather: Weather[],
+    speed: number,
+    deg: number,
+    clouds: number,
+    pop: number,
+    rain?: number
 }
 
 export declare type RoutingConfig = RouteElement[]
