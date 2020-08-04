52cb4d9cb2a9b5fdfd8a99883e67973b1c840278
Mariusz <siewruk.mariusz92@gmail.com>
2020-08-04 10:36:44 +0200

    Tweaked Search
    - changed default export to export

--- src/common/routingConfig.ts
+++ src/common/routingConfig.ts
@@ -1,4 +1,4 @@
-import Search from 'pages/search'
+import { Search } from 'pages/search'
 import { Forecast } from 'pages/forecast'
 import { DetailedForecast } from 'pages/detailedForecast'
 import { RoutingConfig } from './types'
--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -3,11 +3,9 @@ import { Searchbar } from 'modules/searchbar'
 
 import './Search.scss'
 
-const Search = () => (
+export const Search = () => (
   <div className="search-page">
     <h2 className="search-page__header">Enter a City and State</h2>
     <Searchbar display="vertical" />
   </div>
 )
-
-export default Search
