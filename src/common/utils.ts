844ca2731e15ef3542b142aac52c7e1139978f70
Mariusz <siewruk.mariusz92@gmail.com>
2020-08-04 10:30:55 +0200

    Added type definition for returned value

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,6 +1,6 @@
 import { format } from 'date-fns'
 
-export const formatDate = (unixDate: number) => {
+export const formatDate = (unixDate: number): string => {
   const date = new Date(unixDate * 1000)
   const formattedDate = format(date, 'EEEE, LLL d')
   return formattedDate
